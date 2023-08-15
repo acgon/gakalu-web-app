a54e507034a1cca380d4bafd52af300c2600a8c0
acgon <ac.moncken@gmail.com>
2023-08-14 23:54:17 -0300

    Finalizada a página home.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,21 @@
-import { CouponArea, CouponColumn, CouponImg, CouponText, CouponDarkText, HomeDiv, ItemLabel } from "./style";
+import { CouponArea, CouponColumn, CouponImg, CouponText, CouponDarkText, HomeDiv, ItemLabel, Carousel, CarouselItem, CarouselImg, CarouselItemText, RightAlignedLink } from "./style";
 
 import HeaderImage from "../../components/HeaderImage";
 import WhiteLogo from "../../components/Logo";
 import SearchBar from "../../components/SearchBar";
 import couponImg from "../../assets/cupom_icone.png";
 
+import img_sonic from "../../assets/jogos/sonic.png";
+import img_double_dragon from "../../assets/jogos/double_dragon.png";
+import img_gravity_circuit from "../../assets/jogos/gravity_circuit.png";
+import img_armored_core from "../../assets/jogos/armored_core.png";
+
 export default function Home() {
     return (
         <HomeDiv>
             <HeaderImage/>
-            <SearchBar/> <br/>
+            <SearchBar/>
+            <RightAlignedLink href="#">Carrinho</RightAlignedLink>
             <WhiteLogo/> <br/>
 
             <ItemLabel>Cupons disponíveis</ItemLabel>
@@ -23,7 +29,109 @@ export default function Home() {
             </CouponArea>
 
             <ItemLabel>Novidades</ItemLabel>
-            
+
+            <Carousel>
+
+                <CarouselItem>
+                    <CarouselImg src={img_sonic}/>
+                    <CarouselItemText>Sonic Frontiers</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_double_dragon}/>
+                    <CarouselItemText>Double Dragon Gaiden</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_gravity_circuit}/>
+                    <CarouselItemText>Gravity Circuit</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_armored_core}/>
+                    <CarouselItemText>Armored Core VI</CarouselItemText>
+                </CarouselItem>
+
+            </Carousel>
+
+            <ItemLabel>Em alta</ItemLabel>
+
+            <Carousel>
+
+                <CarouselItem>
+                    <CarouselImg src={img_sonic}/>
+                    <CarouselItemText>Sonic Frontiers</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_double_dragon}/>
+                    <CarouselItemText>Double Dragon Gaiden</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_gravity_circuit}/>
+                    <CarouselItemText>Gravity Circuit</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_armored_core}/>
+                    <CarouselItemText>Armored Core VI</CarouselItemText>
+                </CarouselItem>
+
+            </Carousel>
+
+            <ItemLabel>Recomendações</ItemLabel>
+
+            <Carousel>
+
+                <CarouselItem>
+                    <CarouselImg src={img_sonic}/>
+                    <CarouselItemText>Sonic Frontiers</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_double_dragon}/>
+                    <CarouselItemText>Double Dragon Gaiden</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_gravity_circuit}/>
+                    <CarouselItemText>Gravity Circuit</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_armored_core}/>
+                    <CarouselItemText>Armored Core VI</CarouselItemText>
+                </CarouselItem>
+
+            </Carousel>
+
+            <ItemLabel>Outros anúncios</ItemLabel>
+
+            <Carousel>
+
+                <CarouselItem>
+                    <CarouselImg src={img_sonic}/>
+                    <CarouselItemText>Sonic Frontiers</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_double_dragon}/>
+                    <CarouselItemText>Double Dragon Gaiden</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_gravity_circuit}/>
+                    <CarouselItemText>Gravity Circuit</CarouselItemText>
+                </CarouselItem>
+
+                <CarouselItem>
+                    <CarouselImg src={img_armored_core}/>
+                    <CarouselItemText>Armored Core VI</CarouselItemText>
+                </CarouselItem>
+
+            </Carousel>
+
         </HomeDiv>
     )
 }
--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -5,13 +5,31 @@ export const HomeDiv = styled.div `
     display: flex;
     flex-direction: column;
     width: 50vh;
-    height: 100vh;
+    height: 100%;
+    overflow-y: auto;
 
     background: black;
     color: white;
 
     align-items: center;
 
+`;
+
+export const RightAlignedLink = styled.a `
+    text-decoration: none;
+    color: white;
+
+    align-self: flex-end;
+    margin: 5px 5% 0 0;
+
+    font-weight: 400;
+    font-size: 20px;
+
+    &:hover {
+        color: ${global.colors.turquesa};
+        font-weight: 700;
+        
+    };
 `;
 
 export const ItemLabel = styled.h1 `
@@ -51,4 +69,34 @@ export const CouponText = styled.text `
 export const CouponDarkText = styled.text `
     color: ${global.colors.cinzaMedio};
     font-size: 16px;
+`;
+
+export const Carousel = styled.div `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 20%;
+    width: 100%;
+    overflow-x: auto;
+
+    &::-webkit-scrollbar {
+        background: ${global.colors.cinzaEscuro};
+    }
+`;
+
+export const CarouselItem = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 20px 0 20px;
+`;
+
+export const CarouselImg = styled.img `
+    width: 93px;
+    height: 93px;
+`;
+
+export const CarouselItemText = styled.text `
+    font-weight: 300;
+    font-size: 12px;
 `;
